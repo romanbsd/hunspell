81995f4889e01e6795e478c53885f6b99171acd9
Roman Shterenzon <roman.shterenzon@gmail.com>
2012-02-10 23:46:16 +0200

    Support for libhunspell-1.3

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,6 +1,6 @@
 require 'mkmf'
 
-unless have_header('hunspell/hunspell.h') and have_library('hunspell-1.2')
+unless have_header('hunspell/hunspell.h') and (have_library('hunspell-1.2') or have_library('hunspell-1.3'))
   raise 'Hunspell not found.'
 end
 
